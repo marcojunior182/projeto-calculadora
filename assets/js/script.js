7815e32b0a9c8293166d6496b667b0fd4fd53bc0
= <tombombadil182@gmail.com>
2017-06-18 15:27:57 -0300

    Finalizando projeto com JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
-function botao{
-document.form-calculadora.ImprimirValor.value
+function botao(numero){
+
+  var aux = document.formcalc.ImprimirValor.value;
+  document.formcalc.ImprimirValor.value = aux + numero;
+  var resul = document.formcalc.ImprimirValor.value = aux + numero;
+
+}
+
+function limparValor(){
+
+  document.formcalc.ImprimirValor.value = '';
+
+}
+
+function resultadoCalc(){
+  var resul = document.formcalc.ImprimirValor.value;
+  resultado = eval(resul);
+  console.log(resultado)
+  document.formcalc.ImprimirValor.value = resultado;
 }
